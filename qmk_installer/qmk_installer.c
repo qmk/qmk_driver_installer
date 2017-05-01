779b10ab77102a628e6acebea02c93e075a43868
Fred Sundvik <fsundvik@gmail.com>
2017-05-01 23:42:52 +0300

    Actual driver installation
    Kiibohd DFU bootloader is supported

--- qmk_installer/qmk_installer.c
+++ qmk_installer/qmk_installer.c
@@ -35,61 +35,113 @@
 #define DEFAULT_DIR "usb_driver"
 
 int __cdecl main(int argc, char** argv) {
-	static struct wdi_device_info *ldev, dev = {NULL, 0, 0, FALSE, 0, "", NULL, NULL, NULL};
-	static struct wdi_options_create_list ocl = { 0 };
-	static struct wdi_options_prepare_driver opd = { 0 };
-	static struct wdi_options_install_driver oid = { 0 };
-	static struct wdi_options_install_cert oic = { 0 };
-	static int opt_silent = 0, opt_extract = 0, log_level = WDI_LOG_LEVEL_WARNING;
-	static BOOL matching_device_found;
-	int r;
-	char *inf_name = INF_NAME;
-	char *ext_dir = DEFAULT_DIR;
-	char *cert_name = NULL;
 
-	ocl.list_all = TRUE;
-	ocl.list_hubs = TRUE;
-	ocl.trim_whitespaces = TRUE;
-	opd.driver_type = WDI_WINUSB;
+    static int opt_silent = 0, opt_extract = 0, log_level = WDI_LOG_LEVEL_WARNING;
+    wdi_set_log_level(log_level);
+    FILE* file = fopen("drivers.txt", "r");
+    if (!file) {
+        oprintf("Could not open drivers.txt\n");
+        return 1;
+    }
+    char line[1024];
+    const char* delimiters = ",";
+    while (fgets(line, sizeof(line), file)) {
+        if (line[0] == '#')
+            continue;
 
-	wdi_set_log_level(log_level);
+        static struct wdi_device_info *ldev, dev = { NULL, 0, 0, FALSE, 0, "", NULL, NULL, NULL };
+        static struct wdi_options_create_list ocl = { 0 };
+        static struct wdi_options_prepare_driver opd = { 0 };
+        static struct wdi_options_install_driver oid = { 0 };
+        static struct wdi_options_install_cert oic = { 0 };
+        static BOOL matching_device_found;
+        int r;
+        char *inf_name = INF_NAME;
+        char *ext_dir = DEFAULT_DIR;
+        char *cert_name = NULL;
 
-	oprintf("Extracting driver files...\n");
-	r = wdi_prepare_driver(&dev, ext_dir, inf_name, &opd);
-	oprintf("  %s\n", wdi_strerror(r));
-	if ((r != WDI_SUCCESS) || (opt_extract))
-		return r;
+        ocl.list_all = TRUE;
+        ocl.list_hubs = TRUE;
+        ocl.trim_whitespaces = TRUE;
+        char* driver_type = strtok(line, delimiters);
+        if (strcmp(driver_type, "winusb") == 0) {
+            opd.driver_type = WDI_WINUSB;
+        } else if(strcmp(driver_type, "libusb") == 0) {
+            opd.driver_type = WDI_LIBUSB0;
+        }
+        else if (strcmp(driver_type, "libusbk") == 0) {
+            opd.driver_type = WDI_LIBUSBK;
+        } else { 
+            oprintf("Invalid driver type \"%s\"\n", driver_type);
+            oprintf("On the line:\n%s\n", line);
+            return 1;
+        }
 
-	if (cert_name != NULL) {
-		oprintf("Installing certificate '%s' as a Trusted Publisher...\n", cert_name);
-		r = wdi_install_trusted_certificate(cert_name, &oic);
-		oprintf("  %s\n", wdi_strerror(r));
-	}
+        char* desc = strtok(NULL, delimiters);
+        if (!desc) {
+            oprintf("Description missing\n");
+            oprintf("On the line:\n%s\n", line);
+            return 1;
+        }
+        oprintf("Description %s\n", desc);
+        char* str_vid = strtok(NULL, delimiters);
+        if (!str_vid) {
+            oprintf("vid missing\n");
+            oprintf("On the line:\n%s\n", line);
+            return 1;
+        }
 
-	oprintf("Installing driver(s)...\n");
+        char* str_pid = strtok(NULL, delimiters);
+        if (!str_pid) {
+            oprintf("pid missing\n");
+            oprintf("On the line:\n%s\n", line);
+            return 1;
+        }
 
-	// Try to match against a plugged device to avoid device manager prompts
-	matching_device_found = FALSE;
-	if (wdi_create_list(&ldev, &ocl) == WDI_SUCCESS) {
-		r = WDI_SUCCESS;
-		for (; (ldev != NULL) && (r == WDI_SUCCESS); ldev = ldev->next) {
-			if ( (ldev->vid == dev.vid) && (ldev->pid == dev.pid) && (ldev->mi == dev.mi) ) {
-				dev.hardware_id = ldev->hardware_id;
-				dev.device_id = ldev->device_id;
-				matching_device_found = TRUE;
-				oprintf("  %s: ", dev.hardware_id);
-				fflush(stdout);
-				r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-				oprintf("%s\n", wdi_strerror(r));
-			}
-		}
-	}
+        unsigned short vid = (unsigned short)strtol(str_vid, NULL, 16);
+        unsigned short pid = (unsigned short)strtol(str_pid, NULL, 16);
 
-	// No plugged USB device matches this one -> install driver
-	if (!matching_device_found) {
-		r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
-		oprintf("  %s\n", wdi_strerror(r));
-	}
+        dev.vid = vid;
+        dev.pid = pid;
+        dev.desc = desc;
 
-	return r;
+        oprintf("Extracting driver files for %s...\n", desc);
+        r = wdi_prepare_driver(&dev, ext_dir, inf_name, &opd);
+        oprintf("  %s\n", wdi_strerror(r));
+        if ((r != WDI_SUCCESS) || (opt_extract))
+            return r;
+
+        if (cert_name != NULL) {
+            oprintf("Installing certificate '%s' as a Trusted Publisher...\n", cert_name);
+            r = wdi_install_trusted_certificate(cert_name, &oic);
+            oprintf("  %s\n", wdi_strerror(r));
+        }
+
+        oprintf("Installing driver for %s...\n", desc);
+
+        // Try to match against a plugged device to avoid device manager prompts
+        matching_device_found = FALSE;
+        if (wdi_create_list(&ldev, &ocl) == WDI_SUCCESS) {
+            r = WDI_SUCCESS;
+            for (; (ldev != NULL) && (r == WDI_SUCCESS); ldev = ldev->next) {
+                if ((ldev->vid == dev.vid) && (ldev->pid == dev.pid) && (ldev->mi == dev.mi)) {
+                    dev.hardware_id = ldev->hardware_id;
+                    dev.device_id = ldev->device_id;
+                    matching_device_found = TRUE;
+                    oprintf("  %s: ", dev.hardware_id);
+                    fflush(stdout);
+                    r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
+                    oprintf("%s\n", wdi_strerror(r));
+                }
+            }
+        }
+
+        // No plugged USB device matches this one -> install driver
+        if (!matching_device_found) {
+            r = wdi_install_driver(&dev, ext_dir, inf_name, &oid);
+            oprintf("  %s\n", wdi_strerror(r));
+        }
+
+        return r;
+    }
 }
