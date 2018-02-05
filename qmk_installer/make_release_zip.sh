ab097dd5a33965f7baee8065df81b11c92c037f8
skullydazed <skullydazed@gmail.com>
2018-02-05 08:52:28 -0800

    Use the drivers.txt from qmk_firmware

--- qmk_installer/make_release_zip.sh
+++ qmk_installer/make_release_zip.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+rm -f drivers.txt qmk_driver_installer.zip
+wget https://github.com/qmk/qmk_firmware/raw/master/util/drivers.txt
 zip qmk_driver_installer.zip qmk_driver_installer.exe drivers.txt install_all_drivers.bat install_connected_drivers.bat
